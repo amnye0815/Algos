fa13407595db07eb544707d748186a8113c039fa
Austin Nye <amnye0815@gmail.com>
2022-03-15 14:33:38 -0700

    adding Arrays 1 assignment

---
+++ arrays1.js
@@ -0,0 +1,15 @@
+function pushToFront(arr, val) {
+    // Put value in the FRONT of the array. Not your standard .push();
+    var i = 
+}
+
+function popFromFront(arr, val) {
+    // Goal: REMOVE and RETURN the value at i[0] position.
+
+}
+
+function insertAt(arr, val) {
+    // Goal: Insert value into array at a given index.
+}
+
+// BONUS: Will do if I have time.
