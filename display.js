ef6efd6ed5c59a2b9c5b89098ad40f2cdc8ffcfb
Austin Nye <amnye0815@gmail.com>
2022-03-20 12:16:10 -0700

    fronts and display set up

---
+++ display.js
@@ -0,0 +1,8 @@
+// create display
+
+class SLL {
+    // constructor, other methods, removed for brevity
+    display() {
+    	// neatly display nodes in my list
+    }
+}
---
+++ fronts.js
@@ -0,0 +1,22 @@
+// Add front
+class SLL {
+    // constructor, other methods, removed for brevity
+    display() {
+    	// neatly display nodes in my list
+    }
+}
+
+// Remove front
+class SLL {
+    // constructor, other methods, removed for brevity
+    removeFront() {
+    }
+}
+
+// Front
+class SLL {
+    // constructor, other methods, removed for brevity
+    front() {
+
+    }
+}
