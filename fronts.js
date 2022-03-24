45213f87a0367310788a63cc5bc787ef2ae643dd
Austin Nye <amnye0815@gmail.com>
2022-03-24 14:38:20 -0700

    finished

--- fronts.js
+++ fronts.js
@@ -1,22 +1,39 @@
-// Add front
-class SLL {
-    // constructor, other methods, removed for brevity
-    display() {
-    	// neatly display nodes in my list
+// Add front ///////////////////////////////////////////////////////////////////////
+class SLLNode { // Node class
+    constructor(val) {
+        this.value = val; // holds value for node
+        this.next = null; // goes to next node
     }
 }
 
-// Remove front
 class SLL {
-    // constructor, other methods, removed for brevity
+    constructor() {
+    	this.head = null; // head points to first node in SL List.
+    }
+
+    // methods for class SLL listed here
+    addFront(value) {
+        const newNode = new SLLNode(value);
+        newNode.next = this.head; //adding new node to next position at beginning of list.
+        this.head = newNode; // cont of above.
+        return this; // return value at front of list.
+    }
+}
+
+const mySLL = new SLL();
+mySLL.addFront(3);
+mySLL.addFront(15);
+console.log(mySLL);
+
+// Remove front ////////////////////////////////////////////////////////////////////
+class SLL {
     removeFront() {
     }
 }
 
-// Front
-class SLL {
-    // constructor, other methods, removed for brevity
-    front() {
+// // Front /////////////////////////////////////////////////////////////////////////
+// class SLL {
+//     front() {
 
-    }
-}
+//     }
+// }
