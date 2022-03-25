85e01e97f11bd715c4c7fe7e29eb23132d5d306b
Austin Nye <amnye0815@gmail.com>
2022-03-25 12:40:52 -0700

    fronts finished

--- fronts.js
+++ fronts.js
@@ -18,22 +18,35 @@ class SLL {
         this.head = newNode; // cont of above.
         return this; // return value at front of list.
     }
+
+// Remove front ////////////////////////////////////////////////////////////////////
+    removeFront() {
+        if (this.head == null) {
+            return this.head;
+        }
+        const removeNode = this.head; //picking head
+        this.head = removeNode.next; //making head the next node
+        removeNode.next = null; // making the value null (nothing)
+        return this.head; //return the value of null.
+    }
+
+    // // Front ////////////////////////////////////////////////////////////////////////
+    front() {
+        if (this.head == null) {
+            return null;
+        } else {
+            return this.head.value;
+        }
+    }
+
 }
 
-const mySLL = new SLL();
+
+const mySLL = new SLL(); // starting off with an empty list.
+
 mySLL.addFront(3);
 mySLL.addFront(15);
 console.log(mySLL);
-
-// Remove front ////////////////////////////////////////////////////////////////////
-class SLL {
-    removeFront() {
-    }
-}
-
-// // Front /////////////////////////////////////////////////////////////////////////
-// class SLL {
-//     front() {
-
-//     }
-// }
+mySLL.removeFront();
+console.log(mySLL);
+console.log(mySLL.front()); 
